5e42ff38737d61f4207bab0ca0802252223f18eb
saaleh2005 <salehbakhshi614@gmail.com>
2026-06-16 19:30:46 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import json
 import os
-from telegram import Update
+from telegram.ext import CallbackQueryHandler
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 
 # 🔑 گرفتن توکن از Render Environment Variables
@@ -34,9 +35,48 @@ def search_item(text, db):
 
 # ---------- /start ----------
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    keyboard = [
+        [
+            InlineKeyboardButton("🐠 ماهی‌ها", callback_data="fish"),
+            InlineKeyboardButton("🌿 گیاهان", callback_data="plants")
+        ],
+        [
+            InlineKeyboardButton("🐸 دوزیستان", callback_data="amphibians"),
+            InlineKeyboardButton("🦐 بی‌مهرگان", callback_data="inverts")
+        ]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
     await update.message.reply_text(
-        "🌿 Aquarium Bot is online!\nSend fish or plant name."
+        "🌿 به دانشنامه آکواریوم خوش آمدید\n\nیک دسته را انتخاب کنید:",
+        reply_markup=reply_markup
     )
+
+async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    query = update.callback_query
+    await query.answer()
+
+    db = load_db()
+
+    category = query.data
+
+    items = db.get(category, [])
+
+    if not items:
+        await query.edit_message_text("❌ هنوز اطلاعاتی ثبت نشده.")
+        return
+
+    text = ""
+
+    for item in items:
+        text += f"• {item['fa_name']} ({item['en_name']})\n"
+
+    await query.edit_message_text(text)
+
+
 
 
 # ---------- message handler ----------
@@ -83,6 +123,7 @@ def main():
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     print("🤖 Bot is running...")
+    app.add_handler(CallbackQueryHandler(button_handler))
     app.run_polling()
 
 
